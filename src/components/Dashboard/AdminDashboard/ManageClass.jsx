07e3fad76ba3a0a540f88ca461daf3125e290a17
nirjhor saha <nirjhor.badhan25@gmail.com>
2023-06-13 15:26:32 +0600

    load class data using react query

--- src/components/Dashboard/AdminDashboard/ManageClass.jsx
+++ src/components/Dashboard/AdminDashboard/ManageClass.jsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import CustomHeader from '../../../pages/Shared/CustomHeader/CustomHeader';
 import { getAllClasses } from '../../../api/classes';
+import { useQuery } from '@tanstack/react-query';
 
 const ManageClass = () => {
-    const [classes, setClasses] = useState([])
-    useEffect(() => {
-        getAllClasses()
-            .then(data => setClasses(data))
-            .catch(err => console.log(err))
-    },[])
+    // const [classes, setClasses] = useState([])
+
+    // useEffect(() => {
+    //     getAllClasses()
+    //         .then(data => setClasses(data))
+    //         .catch(err => console.log(err))
+    // }, [])
+
+    const { data: classes = [], refetch } = useQuery(['classes'], async () => {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/classes`)
+        return res.json();
+    })
+
     return (
         <div>
             {/* <h1 className='text-center text-4xl font-bold text-orange-500 my-6 border-y-4 w-96 p-4 mx-auto'>Manage Class</h1> */}
             <CustomHeader title='Manage Class'></CustomHeader>
+            
             <div className="grid md:grid-cols-3 gap-4 p-4">
                 {
                     classes.map(singleClass => (
@@ -27,7 +36,7 @@ const ManageClass = () => {
                                 </h2>
                                 <p>Instructor: <span>{singleClass.insName} </span></p>
                                 <p>Email: <span>{singleClass.insEmail} </span></p>
-                                <p>Available Seats: <span>{ singleClass.avSeats }</span></p>
+                                <p>Available Seats: <span>{singleClass.avSeats}</span></p>
                                 <p>Price: $<span>{singleClass.price}</span></p>
                                 <div className="card-actions justify-end">
                                     <button className="btn btn-ghost btn-xs border-2 border-green-400">Approve</button>
@@ -37,8 +46,6 @@ const ManageClass = () => {
                         </div>
                     ))
                 }
-                
-                
             </div>
         </div>
     );
